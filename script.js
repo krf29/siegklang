376a59eea77b48ae1a61168d52a1662a5d1811d6
kraemers <kraemer.meindorf@gmail.com>
2024-05-22 19:53:11 +0200

    Counter fertig

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Dropdown-Menu
+
 const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
@@ -8,3 +10,24 @@ toggleBtn.onclick = function () {
 
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
+
+// Counter
+
+const counts = document.querySelectorAll(".count");
+const speed = 49;
+
+counts.forEach((counter) => {
+  function upData() {
+    const target = Number(counter.getAttribute("data-target"));
+    const count = Number(counter.innerText);
+    const inc = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.floor(inc + count);
+      setTimeout(upData, 15);
+    } else {
+      counter.innerText = target;
+    }
+  }
+  upData();
+});
